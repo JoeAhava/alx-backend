0a516cb8dd6a63b4f530c1803566df8f0b0e9c81
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-05 15:05:55 +0300

    FIFO cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+basic caching
+'''
+
+
+from requests import delete
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''
+    Class implementing a fifo cache
+    '''
+
+    def __init__(self):
+        '''
+        initialize basecache
+        '''
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        puts item in the cache
+        '''
+        if key is None or item is None:
+            return
+        if self.cache_data.get(key, None) != None:
+            self.cache_data[key] = item
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            print('DISCARD: ', list(self.cache_data.keys())[0])
+            del self.cache_data[list(self.cache_data.keys())[0]]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        result = None
+        if key is None:
+            return result
+        try:
+            result = self.cache_data[key]
+        except KeyError:
+            result = None
+        return result
