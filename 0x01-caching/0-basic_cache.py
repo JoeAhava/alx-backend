a61c3a907f1bc59d6017d942f09021c345c0fcad
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-05 14:43:50 +0300

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''
+basic caching
+'''
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''
+    Class implementing a basic cache
+    '''
+
+    def __init__(self):
+        '''
+        initialize basecache
+        '''
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        puts item in the cache
+        '''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        result = None
+        if key is None:
+            return result
+        try:
+            result = self.cache_data[key]
+        except KeyError:
+            result = None
+        return result
